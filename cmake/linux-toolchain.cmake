3d8118c44ffe55d8465e3d30a8b7e6704cb1b8d3
Simon Thorell <kontakt@simonthorell.se>
2025-03-23 16:06:30 +0100

    Update linux toolchain and fix juce npm import path

--- cmake/linux-toolchain.cmake
+++ cmake/linux-toolchain.cmake
@@ -5,7 +5,7 @@ find_package(PkgConfig REQUIRED)
 
 # GTK & WebKit2
 pkg_check_modules(GTK3 REQUIRED gtk+-3.0)
-pkg_check_modules(WEBKIT2GTK REQUIRED webkit2gtk-4.0)
+pkg_check_modules(WEBKIT2GTK REQUIRED webkit2gtk-4.1)
 
 include_directories(${GTK3_INCLUDE_DIRS} ${WEBKIT2GTK_INCLUDE_DIRS})
 link_directories(${GTK3_LIBRARY_DIRS} ${WEBKIT2GTK_LIBRARY_DIRS})

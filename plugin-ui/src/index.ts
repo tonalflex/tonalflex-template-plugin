4c00737c906ebb96ca432a0bf4f291fe8d8ee4e5
Simon Thorell <kontakt@simonthorell.se>
2025-03-24 14:45:49 +0100

    Export types from interface

--- plugin-ui/src/index.ts
+++ plugin-ui/src/index.ts
@@ -2,4 +2,4 @@
 export { default as Plugin } from './components/Plugin.vue';
 
 // Export TypeScript Audio Backend interface
-export type { IAudioBackend } from './backend/IAudioBackend';
+export type * from './backend/IAudioBackend';

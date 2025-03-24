f93a7212c0fc56e9a0d35b72715d5f5a26833269
Simon Thorell <kontakt@simonthorell.se>
2025-03-24 13:23:55 +0100

    Test npm ci

---
+++ plugin-ui/src/index.ts
@@ -0,0 +1,5 @@
+// Export main component
+export { default as Plugin } from './components/Plugin.vue';
+
+// Export TypeScript Audio Backend interface
+export type { IAudioBackend } from './backend/IAudioBackend';

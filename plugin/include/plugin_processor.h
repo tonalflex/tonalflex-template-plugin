bdf0562df4f17b235ae104f44b8f258bbe75d104
Simon Thorell <kontakt@simonthorell.se>
2025-04-03 15:33:32 +0200

    Cleanup juce plugin files

--- plugin/include/plugin_processor.h
+++ plugin/include/plugin_processor.h
@@ -3,9 +3,6 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_dsp/juce_dsp.h>
 #include <juce_gui_basics/juce_gui_basics.h>
-// #include <juce_graphics/juce_graphics.h>
-// #include <juce_audio_utils/juce_audio_utils.h>
-// #include <juce_core/juce_core.h>
 
 namespace audio_plugin {
 class AudioPluginAudioProcessor : public juce::AudioProcessor {
